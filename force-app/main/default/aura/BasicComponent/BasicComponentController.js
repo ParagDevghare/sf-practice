5d916a38841bd3e7512c4aaae15b2a04702334a9
Parag Devghare <49959112+ParagDevghare@users.noreply.github.com>
2021-03-07 18:39:40 +0530

    Child-Parent Component

--- force-app/main/default/aura/BasicComponent/BasicComponentController.js
+++ force-app/main/default/aura/BasicComponent/BasicComponentController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+    getFullName : function(component, event, helper) {
+        let firstName=component.get("v.FirstName");
+        let latsName=component.get("v.LastName");
+        let fullName= firstName +' '+ latsName;
 
+        component.set("v.FullName", fullName);
+    },
+    getpdatableFullName : function(component, event, helper) {
+        let firstName=component.getReference("v.FirstName");
+        let latsName=component.getReference("v.LastName");
+        //let fullName= firstName +' '+ latsName;
+
+        component.set("v.FullName", firstName);
     }
 })
