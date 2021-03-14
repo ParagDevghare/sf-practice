bfd78c0a2cf6763f6b8328bdea0e719c967a0859
Parag Devghare <49959112+ParagDevghare@users.noreply.github.com>
2021-03-14 22:02:37 +0530

    Update

--- force-app/main/default/aura/FlexipageDemo/FlexipageDemoController.js
+++ force-app/main/default/aura/FlexipageDemo/FlexipageDemoController.js
@@ -1,28 +1,5 @@
 ({
-    addContact : function(component, event, helper) {
-          
-           var fName=component.get("v.firstName");
-           var lName=component.get("v.lastName");
-           var action=component.get('c.insertContact');  // Calling Apex Controller method
-           action.setParams({
-             
-               parentAccountId:component.get("v.recordId"),  // Passing parameter
-   
-               firstName1:component.get("v.firstName"),
-   
-               lastName1:component.get("v.lastName")
-              
-           });
-           action.setCallback(this,function(response){
-               var state=response.getState();
-               if(state==="SUCCESS")
-               {
-                   alert('Contact inserted successfully');               
-               }
-              
-           });
-           $A.enqueueAction(action);
-          
+    addContact: function (component, event, helper) {
+        helper.addContactRecord(component, event, helper);
     }
-   
-   })
+})
--- force-app/main/default/aura/FlexipageDemo/FlexipageDemoHelper.js
+++ force-app/main/default/aura/FlexipageDemo/FlexipageDemoHelper.js
@@ -1,5 +1,26 @@
 ({
-    helperMethod : function() {
+    addContactRecord: function (component, event, helper) {
 
+        let fName = component.get("v.firstName1");
+        let lName = component.get("v.lastName1");
+        let action = component.get('c.insertContact');  // Calling Apex Controller method
+        action.setParams({
+
+            parentAccountId: component.get("v.recordId"),  // Passing parameter
+            firstName1: component.get("v.firstName1"),
+            lastName1: component.get("v.lastName1")
+
+        });
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                alert('Contact inserted successfully');
+            }
+            else{
+                let error = response.getError();
+                alert("ERROR :"+JSON.stringify(error));
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
