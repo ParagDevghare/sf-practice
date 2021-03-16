f264c6f4b54e4c5490df01e7af8e1606a527ab51
Parag Devghare <49959112+ParagDevghare@users.noreply.github.com>
2021-03-16 08:59:54 +0530

    handleSubmit

--- force-app/main/default/aura/FlexipageDemo/FlexipageDemoController.js
+++ force-app/main/default/aura/FlexipageDemo/FlexipageDemoController.js
@@ -1,5 +1,8 @@
 ({
     addContact: function (component, event, helper) {
         helper.addContactRecord(component, event, helper);
+    },
+    handleSubmit: function (component, event, helper) {
+        helper.handleSubmit(component, event, helper);
     }
 })
