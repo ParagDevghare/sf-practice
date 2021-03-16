65828f48f4f1eba2178c4f63f79f58cc8c7fed36
Parag Devghare <49959112+ParagDevghare@users.noreply.github.com>
2021-03-16 09:29:14 +0530

    Updated

--- force-app/main/default/aura/FlexipageDemo/FlexipageDemoHelper.js
+++ force-app/main/default/aura/FlexipageDemo/FlexipageDemoHelper.js
@@ -22,5 +22,11 @@
             }
         });
         $A.enqueueAction(action);
+    },
+    handleSubmit: function (cmp, event, helper) {
+        event.preventDefault();       // stop the form from submitting
+        const fields = event.getParam('fields');
+        fields.LastName = 'My Custom Last Name'; // modify a field
+        cmp.find('myRecordForm').submit(fields);
     }
 })
