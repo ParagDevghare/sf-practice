b137d99afe65f2b368da226d6f66d5ff4d73e757
Parag Devghare <49959112+ParagDevghare@users.noreply.github.com>
2021-03-16 09:09:12 +0530

    handleSubmit

--- force-app/main/default/aura/RecordFormDemo/RecordFormDemoController.js
+++ force-app/main/default/aura/RecordFormDemo/RecordFormDemoController.js
@@ -1,5 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-
+    handleSubmit : function(component, event, helper){
+        helper.handleSubmit(component, event, helper;
     }
 })
--- force-app/main/default/aura/RecordFormDemo/RecordFormDemoHelper.js
+++ force-app/main/default/aura/RecordFormDemo/RecordFormDemoHelper.js
@@ -1,5 +1,8 @@
 ({
-    helperMethod : function() {
-
+    handleSubmit: function (cmp, event, helper) {
+        event.preventDefault();       // stop the form from submitting
+        const fields = event.getParam('fields');
+        fields.LastName = 'My Custom Last Name'; // modify a field
+        cmp.find('myRecordForm').submit(fields);
     }
 })
