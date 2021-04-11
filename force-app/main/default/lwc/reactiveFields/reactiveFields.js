ef250af4d8f2dfa67e1f99642690394f318df573
Parag Devghare <49959112+ParagDevghare@users.noreply.github.com>
2021-04-11 12:02:53 +0530

    Reactive fields

--- force-app/main/default/lwc/reactiveFields/reactiveFields.js
+++ force-app/main/default/lwc/reactiveFields/reactiveFields.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class ReactiveFields extends LightningElement {}
+export default class Basic extends LightningElement {
+    firstName = "";
+    lastName = "";
+
+    handleChanges(event) {
+        const fieldName = event.target.name;
+
+        if (fieldName == "firstName") {
+            this.firstName = event.target.value;
+        }
+        else if (fieldName == "lastName") {
+            this.lastName = event.target.value;
+        }
+    }
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+    updateName(event){
+        
+    }
+}
